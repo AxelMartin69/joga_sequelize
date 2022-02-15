39a73af567b08a08e5fae94d6bf25190d2a0be1d
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-02-15 22:47:47 +0200

    Getted article data by slug

--- routes/article.js
+++ routes/article.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const articleCotroller = require('../controllers/article')
 
 router.get('/', articleCotroller.getAllArticles)
+router.get('/article/:slug', articleCotroller.getArticleBySlug)
 
 module.exports = router
